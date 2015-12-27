b11c31cc19d059f90526a1331c0ed7a4b5f5859a
TOR- <tiarnachreidy@gmail.com>
2015-12-27 22:01:46 +0000

    hex support for printf

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -10,6 +10,39 @@ static void print(const char* data, size_t data_length)
     putchar((int) ((const unsigned char*) data)[i]);
 }
 
+#define MAX_INT_SIZE 12
+
+int printint ( int i, int capital, int base )
+{
+  char retbuf[MAX_INT_SIZE];
+  int r = 0, neg = 0, u = i;
+  char *bc;
+  if (i == 0)
+    return 0;
+  else if (i < 0)
+    {
+      u *= -1;
+      neg = 1;
+    }
+  for (int h = 0; h < MAX_INT_SIZE; h++)
+	{
+		retbuf[h]='\0';
+	}
+  bc = retbuf + MAX_INT_SIZE - 1;
+  *--bc = '\0';
+  while (u) {
+		r = u % base;
+		if( r >= 10 )
+			r += capital - '0' - 10;
+		*--bc = r + '0';
+		u /= base;
+	}
+	
+	if(neg) *--bc = '-';
+	print(retbuf, MAX_INT_SIZE);
+	return 0;
+}
+
 int printf(const char* restrict format, ...)
 {
   va_list parameters;
@@ -62,87 +95,13 @@ int printf(const char* restrict format, ...)
 	{
 	  format++;
 	  int d = va_arg(parameters, const int);
-	  if ( d < 0 )
-	    {
-	      print('-', 1);
-	      d *= -1;
-	    }
-	  char s[sizeof d]; //CAUTION Compile time only
-	  itoa(d,s);
-	  print(s, strlen(s));
+	  printint(d, 'a' , 10);
 	}
       else if ( *format == 'x' )
 	{
 	  format++;
-	  /*int hex = va_arg(parameters, int);
-	    char *buf = NULL;
-	    int i=0;
-	    int j=30;
-	    printf("hex");
-	    for(;hex!=0;i++)
-	    {
-	    printf("%d%d\n",i,hex);
-	    switch(hex%16){
-	    case 0x0:
-	    buf[i] = '0';
-	    break;
-	    case 0x1:
-	    buf[i] = '1';
-	    break;
-	    case 0x2:
-	    buf[i] = '2';
-	    break;
-	    case 0x3:
-	    buf[i] = '3';
-	    break;
-	    case 0x4:
-	    buf[i] = '4';
-	    break;
-	    case 0x5:
-	    buf[i] = '5';
-	    break;
-	    case 0x6:
-	    buf[i] = '6';
-	    break;
-	    case 0x7:
-	    buf[i] = '7';
-	    break;
-	    case 0x8:
-	    buf[i] = '8';
-	    break;
-	    case 0x9:
-	    buf[i] = '9';
-	    break;
-	    case 0xA:
-	    buf[i] = 'A';
-	    break;
-	    case 0xB:
-	    buf[i] = 'B';
-	    break;
-	    case 0xC:
-	    buf[i] = 'C';
-	    break;
-	    case 0xD:
-	    buf[i] = 'D';
-	    break;
-	    case 0xE:
-	    buf[i] = 'E';
-	    break;
-	    case 0xF:
-	    buf[i] = 'F';
-	    break;
-	    default:
-	    print("NaN",3);
-	    break;
-	    }
-	    hex = hex/16;
-	    }
-	    i=0;
-	    j=30;
-	    char retbuf[30];
-	    for(;i++,j--;i<30) retbuf[i] = buf[j];
-	    print("0x",2);
-	    print(retbuf, strlen(retbuf));*/
+		int x = va_arg(parameters, const int);
+		printint(x, 'a' , 16);
 	}
       else
 	{
